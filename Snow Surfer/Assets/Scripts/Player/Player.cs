caeef9a7b3928642de14a5852d13ba2b6f336760
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-02-19 11:45:00 +0530

    Player Script for the Surfer

--- Snow Surfer/Assets/Scripts/Player/Player.cs
+++ Snow Surfer/Assets/Scripts/Player/Player.cs
@@ -2,7 +2,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] private SurfaceEffector2D surfaceEffector2D;
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] SurfaceEffector2D surfaceEffector2D;
+    bool canMove = true;
+
+    void Update()
+    {
+        if (canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddTorque(-torqueAmount);
+        }
+    }
 }
+
