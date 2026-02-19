7dcab4e11adcb19a83dc848846dcc5f8bdd3e0da
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-02-19 10:23:25 +0530

    Playersetup for the surface Effector

---
+++ Snow Surfer/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private SurfaceEffector2D surfaceEffector2D;
+    [SerializeField] private Rigidbody2D rb;
+}
